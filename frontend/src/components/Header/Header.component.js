49fb9cf1847b100b26e8f1886389b82e580e3292
disha9112 <disha9112@gmail.com>
2022-07-14 11:58:01 +0530

    responsiveness done

--- frontend/src/components/Header/Header.component.js
+++ frontend/src/components/Header/Header.component.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Header.styles.css";
 
 function Header({ toggleHeader, setToggleHeader }) {
+  const [responsive, setResponsive] = useState(false);
+
+  function handleResponsiveness() {
+    setResponsive(!responsive);
+  }
+
   async function handleLogOutClick() {
     setToggleHeader(false);
     localStorage.removeItem("token");
@@ -16,6 +22,9 @@ function Header({ toggleHeader, setToggleHeader }) {
             <div className="header-brand">Discussion Forum</div>
           </Link>
           <ul className="header-links">
+            <div className="header-menu" onClick={() => handleResponsiveness()}>
+              ☰
+            </div>
             <Link className="link" to="/profile">
               <li>Profile</li>
             </Link>
@@ -30,6 +39,20 @@ function Header({ toggleHeader, setToggleHeader }) {
             </Link>
           </ul>
         </div>
+        <ul className={`${responsive ? "show" : ""} header-links-display`}>
+          <Link className="link" to="/profile">
+            <li>Profile</li>
+          </Link>
+          <Link className="link" to="/ask">
+            <li>Ask</li>
+          </Link>
+          <Link to="/trending" className="link">
+            <li>Trending</li>
+          </Link>
+          <Link onClick={() => handleLogOutClick()} className="link" to="/">
+            <li>Log Out</li>
+          </Link>
+        </ul>
       </div>
     );
   } else {
@@ -40,6 +63,9 @@ function Header({ toggleHeader, setToggleHeader }) {
             <div className="header-brand">Discussion Forum</div>
           </Link>
           <ul className="header-links">
+            <div className="header-menu" onClick={() => handleResponsiveness()}>
+              ☰
+            </div>
             <Link className="link" to="/">
               <li>About Us</li>
             </Link>
@@ -48,6 +74,14 @@ function Header({ toggleHeader, setToggleHeader }) {
             </Link>
           </ul>
         </div>
+        <ul className={`${responsive ? "show" : ""} header-links-display`}>
+          <Link className="link" to="/">
+            <li>About Us</li>
+          </Link>
+          <Link className="link" to="/">
+            <li>Contact</li>
+          </Link>
+        </ul>
       </div>
     );
   }
